eb85dc472dfaaa6c521f5408b0bbd5af6149fefd
Peter <peter.ping666@gmail.com>
2020-06-07 03:09:42 -0700

    update

--- src/_157_ReadNCharactersGivenRead4.java
+++ src/_157_ReadNCharactersGivenRead4.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class _157_ReadNCharactersGivenRead4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    public int read(char[] buf, int n) {
+        boolean eof = false;
+        char[] tmp = new char[4];
+        int total = 0;
+        while(!eof && total < n) {
+            int count = read4(tmp);
+            count = Math.min(count, n - total);
+            eof = count < 4;
+            for(int i = 0; i < count; i++) {
+                buf[total++] = tmp[i];
+            }
+        }
+        return total;
+    }
+
+    int read4(char[] tmp) {
+        return 0;
+    }
 }
