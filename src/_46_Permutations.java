3021c1cf313c078f10099b41dd7670ea633d89d7
Peter <peter.ping666@gmail.com>
2020-06-23 12:21:04 -0700

    update

---
+++ src/_46_Permutations.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class _46_Permutations {
+    /**
+     * Time O(n! * n)
+     * Space O(n!)
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute1(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        helper(list, new ArrayList<>(), nums);
+        return list;
+    }
+
+    private void helper(List<List<Integer>> list, List<Integer> path, int[] nums) {
+        if(path.size() == nums.length) {
+            list.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i = 0; i < nums.length; i++) {
+            if(path.contains(nums[i])) continue;
+            path.add(nums[i]);
+            helper(list, path, nums);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    /**
+     * Time O(n! - n! * n)
+     * Space O(n!)
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute2(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        helper(list, nums, 0);
+        return list;
+    }
+
+    private void helper(List<List<Integer>> list, int[] nums, int start) {
+        if(start == nums.length) {
+            List<Integer> path = new ArrayList<>();
+            for(int num : nums) {
+                path.add(num);
+            }
+            list.add(path);
+        }
+        for(int i = start; i < nums.length; i++) {
+            swap(nums, start, i);
+            helper(list, nums, start + 1);
+            swap(nums, start, i);
+        }
+    }
+
+    private void swap(int[] nums, int l, int r) {
+        int temp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = temp;
+    }
+
+    /**
+     * Time O(n! - n * n!)
+     * Space O(n!)
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute3(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        for(int num : nums) {
+            path.add(num);
+        }
+        helper(list, path, nums.length, 0);
+        return list;
+    }
+
+    private void helper(List<List<Integer>> list, List<Integer> path, int n, int start) {
+        if(start == n) {
+            list.add(new ArrayList<>(path));
+        }
+        for(int i = start; i < n; i++) {
+            Collections.swap(path, start, i);
+            helper(list, path, n, start + 1);
+            Collections.swap(path, start, i);
+        }
+    }
+}
---
+++ src/_47_PermutationsII.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class _47_PermutationsII {
+    /**
+     * Time O(n!)
+     * Space O(n!)
+     * @param nums
+     * @return
+     */
+        public List<List<Integer>> permuteUnique1(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        for(int num : nums) {
+            path.add(num);
+        }
+
+        helper(list, path, nums.length, 0);
+        return list;
+    }
+
+    private void helper(List<List<Integer>> list, List<Integer> path, int n, int start) {
+        if(start == n) {
+            list.add(new ArrayList<>(path));
+        }
+        HashSet<Integer> seen = new HashSet<>();
+        for(int i = start; i < n; i++) {
+            if(seen.add(path.get(i))) {
+                Collections.swap(path, start, i);
+                helper(list, path, n, start + 1);
+                Collections.swap(path, start, i);
+            }
+        }
+    }
+
+    /**
+     * Time O(n!)
+     * Space O(n)
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permuteUnique2(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        Arrays.sort(nums);
+        helper(list, new ArrayList<>(), nums, used);
+        return list;
+    }
+
+    private void helper(List<List<Integer>> list,
+                        List<Integer> path,
+                        int[] nums, boolean[] used) {
+        if(path.size() == nums.length) {
+            list.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i = 0; i < nums.length; i++) {
+            if(used[i] || i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+            used[i] = true;
+            path.add(nums[i]);
+            helper(list, path, nums, used);
+            used[i] = false;
+            path.remove(path.size() - 1);
+        }
+    }
+}
