283eeb13af22f4a403847628d980797f49bc512f
Peter <peter.ping666@gmail.com>
2020-06-22 19:14:39 -0700

    update

---
+++ src/_524_LongestWordinDictionarythroughDeleting.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class _524_LongestWordinDictionarythroughDeleting {
+
+    /**
+     * Time O()
+     * Space O()
+     * @param s
+     * @param d
+     * @return
+     */
+    public String findLongestWord(String s, List<String> d) {
+        String max_str = "";
+        for (String str: d) {
+            if (isSubsequence(str, s)) {
+                if (str.length() > max_str.length()
+                        || (str.length() == max_str.length() && str.compareTo(max_str) < 0))
+                    max_str = str;
+            }
+        }
+        return max_str;
+    }
+    private boolean isSubsequence(String x, String y) {
+        int j = 0;
+        for (int i = 0; i < y.length() && j < x.length(); i++)
+            if (x.charAt(j) == y.charAt(i))
+                j++;
+        return j == x.length();
+    }
+}
