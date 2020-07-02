88a5de16c304ddba1aa09665737bdccf536e386a
Peter <peter.ping666@gmail.com>
2020-07-01 18:59:29 -0700

    update

--- src/_316_RemoveDuplicateLetters.java
+++ src/_316_RemoveDuplicateLetters.java
@@ -10,26 +10,13 @@ public class _316_RemoveDuplicateLetters {
      * @return
      */
     public String removeDuplicateLetters(String s) {
-
         Stack<Character> stack = new Stack<>();
-
-        // this lets us keep track of what's in our solution in O(1) time
         HashSet<Character> seen = new HashSet<>();
-
-        // this will let us know if there are any more instances of s[i] left in s
         HashMap<Character, Integer> last_occurrence = new HashMap<>();
         for(int i = 0; i < s.length(); i++) last_occurrence.put(s.charAt(i), i);
-
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
-            // we can only try to add c if it's not already in our solution
-            // this is to maintain only one of each character
             if (!seen.contains(c)){
-                // if the last letter in our solution:
-                //     1. exists
-                //     2. is greater than c so removing it will make the string smaller
-                //     3. it's not the last occurrence
-                // we remove it from the solution to keep the solution optimal
                 while(!stack.isEmpty() && c < stack.peek() && last_occurrence.get(stack.peek()) > i){
                     seen.remove(stack.pop());
                 }
@@ -38,7 +25,7 @@ public class _316_RemoveDuplicateLetters {
             }
         }
         StringBuilder sb = new StringBuilder(stack.size());
-        for (Character c : stack) sb.append(c.charValue());
+        for (char c : stack) sb.append(c);
         return sb.toString();
     }
 }
