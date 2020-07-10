d7f5acc1b19046bf70f21a69b39e7ea88569f5d1
Peter <peter.ping666@gmail.com>
2020-07-09 21:00:12 -0700

    400

--- src/QuickSort/QuickSort.java
+++ src/QuickSort/QuickSort.java
@@ -1,74 +1,49 @@
 package QuickSort;
 
 public class QuickSort {
-    /* This function takes last element as pivot,
-       places the pivot element at its correct
-       position in sorted array, and places all
-       smaller (smaller than pivot) to left of
-       pivot and all greater elements to right
-       of pivot */
-    int partition(int arr[], int low, int high)
-    {
-        int pivot = arr[high];
-        int i = (low-1); // index of smaller element
-        for (int j=low; j<high; j++)
-        {
-            // If current element is smaller than the pivot
-            if (arr[j] < pivot)
-            {
-                i++;
+    public int[] quickSort(int[] array) {
+        if(array == null) {
+            return array;
+        }
+        quickSort(array, 0, array.length - 1);
+        return array;
+    }
 
-                // swap arr[i] and arr[j]
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+    private void quickSort(int[] array, int left, int right) {
+        if(left >= right) {
+            return;
+        }
+        int pivotPos = partition(array, left, right);
+        quickSort(array, left, pivotPos - 1);
+        quickSort(array, pivotPos + 1, right);
+    }
+
+    private int partition(int[] array, int left, int right) {
+        int pivotIndex = pivotIndex(left, right);
+        int pivot = array[pivotIndex];
+        swap(array, pivotIndex, right);
+        int leftBound = left;
+        int rightBound = right - 1;
+        while(leftBound <= rightBound) {
+            if(array[leftBound] < pivot) {
+                leftBound++;
+            } else if(array[rightBound] >= pivot) {
+                rightBound--;
+            } else {
+                swap(array, leftBound++, rightBound--);
             }
         }
-
-        // swap arr[i+1] and arr[high] (or pivot)
-        int temp = arr[i+1];
-        arr[i+1] = arr[high];
-        arr[high] = temp;
-
-        return i+1;
+        swap(array, leftBound, right);
+        return leftBound;
     }
 
-    /* The main function that implements QuickSort()
-      arr[] --> Array to be sorted,
-      low  --> Starting index,
-      high  --> Ending index */
-    void sort(int arr[], int low, int high)
-    {
-        if (low < high)
-        {
-            /* pi is partitioning index, arr[pi] is
-              now at right place */
-            int pi = partition(arr, low, high);
-
-            // Recursively sort elements before
-            // partition and after partition
-            sort(arr, low, pi-1);
-            sort(arr, pi+1, high);
-        }
+    private int pivotIndex(int left, int right) {
+        return left + (int)(Math.random() * (right - left + 1));
     }
 
-    static void printArray(int arr[])
-    {
-        int n = arr.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(arr[i]+" ");
-        System.out.println();
+    private void swap(int[] array, int left, int right) {
+        int tmp = array[left];
+        array[left] = array[right];
+        array[right] = tmp;
     }
-
-//    public static void main(String args[])
-//    {
-//        int arr[] = {10, 7, 8, 9, 1, 5};
-//        int n = arr.length;
-//
-//        QuickSort ob = new QuickSort();
-//        ob.sort(arr, 0, n-1);
-//
-//        System.out.println("sorted array");
-//        printArray(arr);
-//    }
 }
