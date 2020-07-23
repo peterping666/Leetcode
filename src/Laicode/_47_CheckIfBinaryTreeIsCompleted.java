13500ebd7dac22a9c5ef60a03c91ca455c953011
Peter <peter.ping666@gmail.com>
2020-07-22 23:04:07 -0700

    laicode

---
+++ src/Laicode/_47_CheckIfBinaryTreeIsCompleted.java
@@ -0,0 +1,40 @@
+package Laicode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class _47_CheckIfBinaryTreeIsCompleted {
+
+    public class Solution {
+        public boolean isCompleted(TreeNode root) {
+            if(root == null) {
+                return true;
+            }
+            Queue<TreeNode> q = new LinkedList<>();
+            q.offer(root);
+            boolean flag = false;
+            while(!q.isEmpty()) {
+                int size = q.size();
+                for(int i = 0; i < size; i++) {
+                    TreeNode cur = q.poll();
+                    if(cur.left == null) {
+                        flag = true;
+                    } else if(flag) {
+                        return false;
+                    } else {
+                        q.offer(cur.left);
+                    }
+                    if(cur.right == null) {
+                        flag = true;
+                    } else if(flag) {
+                        return false;
+                    } else {
+                        q.offer(cur.right);
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
+}
